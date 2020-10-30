bd76af5211bd33bcb7b192b3273d149051bc255b
Anderson Angulo <andersonangulo1704@gmail.com>
2020-10-30 13:50:08 -0500

    Crea todos los componentes de las paginas privadas de la aplicacion

---
+++ src/components/pages/Course.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Course = () => {
+  return (
+    <div className="ed-grid">
+      <h1>Teachers</h1>
+    </div>
+  )
+}
+
+export default Course
---
+++ src/components/pages/Speciality.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const Speciality = () => {
+  return (
+    <div className="ed-grid">
+      <h1>Speciality</h1>
+    </div>
+  )
+}
+export default Speciality
